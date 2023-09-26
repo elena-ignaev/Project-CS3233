326f7746a8d41da471514f59a817c173cf07c0a4
elena-ignaev <ngkhanhvy11@gmail.com>
2023-09-27 07:03:10 +0800

    start working on home page

--- src/main/java/com/example/projectp1/Controller/HomeController.java
+++ src/main/java/com/example/projectp1/Controller/HomeController.java
@@ -1,7 +1,44 @@
 package com.example.projectp1.Controller;
 
+import javafx.application.HostServices;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
 public class HomeController {
+    Stage stage;
+    @FXML
+    private Button newSpace;
+
+    @FXML
+    private Button experiment1;
+
+    @FXML
+    private Button experiment2;
+
+    @FXML
+    private Button newNote;
+
+    @FXML
+    private Button safetyPrecautions;
+
+    @FXML
+    private Button QAnotes;
+
+    public void createNew() {
 
 
+    }
+
+    public void accessNote() {
+
+    }
+
+    public void accessSpace() {
+
+    }
 
 }
